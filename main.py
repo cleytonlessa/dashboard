d8cca0408c9f15fee8a917290e04e93c3a3e2ee8
Cleyton Lessa <cleytonlessa@gmail.com>
2022-09-05 17:51:27 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,59 @@
+# https://dash.plotly.com
+# pip install pandas
+# pip install openpyxl
+# pip install dash
+
+# Run this app with `python app.py` and
+# visit http://127.0.0.1:8050/ in your web browser.
+
+from dash import Dash, html, dcc, Input, Output
+import plotly.express as px
+import pandas as pd
+
+app = Dash(__name__)
+
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+df = pd.read_excel("Vendas.xlsx")
+
+fig = px.bar(df, x="Produto", y="Quantidade", color="ID Loja", barmode="group")
+
+opcoes = list(df['ID Loja'].unique())
+opcoes.append("Todas as Lojas")
+
+app.layout = html.Div(children=[
+    html.H1(children='Faturamento das Lojas'),
+    html.H2(
+        children='Gráfico com o faturamento de todos os produtos separados por loja'),
+
+    html.Div(children='''
+        Obs: Esse gráfico mostra a quantidade de produtos vendidos, não o faturamento.
+    '''),
+    html.Div(id='texto'),
+
+    dcc.Dropdown(opcoes, value='Todas as Lojas', id='lista_lojas'),
+
+    dcc.Graph(
+        id='grafico_quantidade_vendas',
+        figure=fig
+    )
+])
+
+
+@app.callback(
+    Output('grafico_quantidade_vendas', 'figure'),
+    Input('lista_lojas', 'value')
+)
+def update_output(value):
+    if value == "Todas as Lojas":
+        fig = px.bar(df, x="Produto", y="Quantidade",
+                     color="ID Loja", barmode="group")
+    else:
+        tabela_filtrada = df.loc[df['ID Loja'] == value, :]
+        fig = px.bar(tabela_filtrada, x="Produto", y="Quantidade",
+                     color="ID Loja", barmode="group")
+    return fig
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
